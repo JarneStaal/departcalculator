79bbcbc68af0c5cede87efacb127606fb336f398
JarneSt <12001144@student.pxl.be>
2022-04-12 09:55:08 +0200

    button style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,62 +2,73 @@
   <div class="w-50 m-auto">
     <div class="form-group">
       <h6>Aankomsttijd</h6>
-      <input type="time" class="form-control text-center" v-model="aankomsttijd" @change="clearVertrektijd">
+      <input
+        type="time"
+        class="form-control text-center"
+        v-model="aankomsttijd"
+        @change="clearVertrektijd"
+      />
     </div>
     <div class="form-group mt-5">
       <h6>Duurtijd</h6>
-      <input type="time" class="form-control text-center" v-model="duurtijd" @change="clearVertrektijd">
+      <input
+        type="time"
+        class="form-control text-center"
+        v-model="duurtijd"
+        @change="clearVertrektijd"
+      />
     </div>
-    <button class="btn btn-primary mt-5" @click="berekenTijd">Bereken vertrektijd</button>
+    <n-button class="mt-5" @click="berekenTijd">Bereken vertrektijd</n-button>
   </div>
 
   <div v-if="vertrektijd !== null" class="mt-5">
-    <h2>Vertrektijd is {{this.vertrektijd}}</h2>
+    <h2>Vertrektijd is {{ this.vertrektijd }}</h2>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { NButton } from "naive-ui";
 export default {
-  name: 'Home',
-  components: {},
-  data(){
-    return {
-      aankomsttijd : null,
-      duurtijd : null,
-      vertrektijd : null
-    }
+  name: "Home",
+  components: {
+    NButton,
   },
-  methods : {
-    berekenTijd(){
-      var num = Math.abs(this.differenceInMinutes(this.aankomsttijd, this.duurtijd));
-      var hours = (num / 60);
+  data() {
+    return {
+      aankomsttijd: null,
+      duurtijd: null,
+      vertrektijd: null,
+    };
+  },
+  methods: {
+    berekenTijd() {
+      var num = Math.abs(
+        this.differenceInMinutes(this.aankomsttijd, this.duurtijd)
+      );
+      var hours = num / 60;
       var rhours = Math.floor(hours);
       var minutes = (hours - rhours) * 60;
       var rminutes = Math.round(minutes);
-      if (rminutes <= 9){
+      if (rminutes <= 9) {
         rminutes = `0${rminutes}`;
-      }
-      else {
+      } else {
         rminutes = rminutes;
       }
 
-      this.vertrektijd = `${rhours}:${rminutes}`
+      this.vertrektijd = `${rhours}:${rminutes}`;
     },
-    differenceInMinutes(start,end){
-      var totalMinutes = function(value) {
-        var match = (/(\d{1,2}):(\d{1,2})/g).exec(value);
-        return (Number(match[1]) * 60) + Number(match[2]);
-      }
+    differenceInMinutes(start, end) {
+      var totalMinutes = function (value) {
+        var match = /(\d{1,2}):(\d{1,2})/g.exec(value);
+        return Number(match[1]) * 60 + Number(match[2]);
+      };
       return totalMinutes(end) - totalMinutes(start);
     },
-    clearVertrektijd(){
+    clearVertrektijd() {
       this.vertrektijd = null;
-    }
+    },
   },
-  computed : {
-
-  }
-}
+  computed: {},
+};
 </script>
